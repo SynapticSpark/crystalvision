e3bc972024aac3e667fc5e6fd741d7247c4f2127
SynapticSpark <spencergilbert21@gmail.com>
2024-11-11 19:28:10 -0500

    Crystal Vision

--- src/main/java/com/crystalvision/CrystalVisionConfig.java
+++ src/main/java/com/crystalvision/CrystalVisionConfig.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.crystalvision;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
 @ConfigGroup("example")
-public interface ExampleConfig extends Config
+public interface CrystalVisionConfig extends Config
 {
 	@ConfigItem(
 		keyName = "greeting",
--- src/main/java/com/crystalvision/CrystalVisionPlugin.java
+++ src/main/java/com/crystalvision/CrystalVisionPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.crystalvision;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Crystal Vision"
 )
-public class ExamplePlugin extends Plugin
+public class CrystalVisionPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private CrystalVisionConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	CrystalVisionConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(CrystalVisionConfig.class);
 	}
 }
